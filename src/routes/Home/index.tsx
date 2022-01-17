c988d7873b1db639bb23fc5bf0d4a05a1787a531
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2022-01-17 19:26:23 +0300

    Removing sketch pages and components

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import SiteHeader from '../../components/partials/Header';
-import HomeBanner from '../../components/partials/Home/Banner';
-import ContentItem from '../../components/partials/Home/ContentItem';
-import SiteFooter from '../../components/partials/Footer';
 
 const Home = function () {
   return (
     <div className="Home">
-      <SiteHeader />
-      <HomeBanner />
-      <ContentItem align="left" />
-      <ContentItem align="right" />
-      <ContentItem align="left" />
-      <SiteFooter />
+      <p>Home Works!</p>
     </div>
   );
 };
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,29 +5,11 @@ import { Spin } from 'antd';
 import Home from './Home';
 
 const About = lazy(() => import('./About'));
-const Profile = lazy(() => import('./Profile'));
-const ProfileEdit = lazy(() => import('./Profile/Edit'));
 
 const Router: FunctionComponent = function () {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route
-        path="profile"
-        element={
-          <Suspense fallback={<Spin />}>
-            <Profile />
-          </Suspense>
-        }
-      />
-      <Route
-        path="profile/edit"
-        element={
-          <Suspense fallback={<Spin />}>
-            <ProfileEdit />
-          </Suspense>
-        }
-      />
       <Route
         path="about"
         element={
