d28190a5aab5064acd1853c83e8b18c56b934793
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2022-01-18 00:56:18 +0300

    Add home profiles

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SiteHeader from '../../components/Shareds/SiteHeader';
 import HomeBanner from '../../components/Partials/Home/Banner';
+import HomeProfiles from '../../components/Partials/Home/Profiles';
 
 const Home = function () {
   return (
     <div className="Home">
       <SiteHeader />
       <HomeBanner />
+      <HomeProfiles />
     </div>
   );
 };
