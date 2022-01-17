94ca18445ea98b920b65a3a8719dbbac354d3eb1
Erdoğan Bulut <e-bulut@hotmail.com.tr>
2022-01-18 00:34:40 +0300

    Add home banner

---
+++ src/components/Partials/Home/Banner/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+import './Banner.scss';
+
+const HomeBanner = function () {
+  return (
+    <section className="component is-home-banner">
+      <div className="home-banner--bg" style={{ backgroundImage: `url(/home--banner.jpg)` }}>
+        <div className="home-banner--overlay">
+          <div className="container home-banner--container">
+            <div className="home-banner--content">
+              <h1>WE ARE AWESOME</h1>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore sapiente illo
+                assumenda corporis temporibus odit laborum dicta placeat perferendis nesciunt!
+                Neque, nemo harum. Esse magni perspiciatis sunt. Odio, quas vitae.
+              </p>
+              <Link to="/">
+                <Button type="primary" shape="round" size="large">
+                  Apply Now!
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeBanner;
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SiteHeader from '../../components/Shareds/SiteHeader';
+import HomeBanner from '../../components/Partials/Home/Banner';
 
 const Home = function () {
   return (
     <div className="Home">
       <SiteHeader />
+      <HomeBanner />
     </div>
   );
 };
